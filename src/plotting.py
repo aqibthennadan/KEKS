71a2a1d0c5e046742c84e40095323673438a5788
Aqib Thennadan <aqib.thennadan@hs-offenburg.de>
2026-08-04 14:48:29 +0200

    modified plot_EnBalance, to merge LT and HT thermal buses

--- src/plotting.py
+++ src/plotting.py
@@ -1,17 +1,26 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def plot_EnBalance(n, carrier, ax, bus_sizes, bus_scale = 3e-10, title = 'En balance'):
+def plot_EnBalance(network, carrier, ax, bus_sizes, bus_scale = 3e-10, title = 'En balance'):
 
     #filtering buses and bus sizes for the carrier of interest
-    cr_buses = n.buses[n.buses.carrier == carrier].index
+    cr_buses = network.buses[network.buses.carrier == carrier].index
     bus_sizes_cr = bus_sizes.loc[bus_sizes.index.get_level_values(0).isin(cr_buses)]
     
     #Setting link width only for carrier of interest, and the remaining zero
-    link_width = pd.Series(0.0, index=n.links.index)
-    link_width[n.links['carrier'] == carrier] = 1.5
+    link_width = pd.Series(0.0, index=network.links.index)
+    link_width[network.links['carrier'] == carrier] = 1.5
+    if carrier == 'thermal':
+        link_width[network.links['carrier'] == 'DHN'] = 1.5
+ 
+    #Merging LT and HT into a single thermal bus, renaming HT to LT, and summing the sizes
+    bus_sizes_cr.index = pd.MultiIndex.from_tuples([
+        (bus.replace('_HT', '_LT'), tech) for bus, tech in bus_sizes_cr.index
+    ], names = bus_sizes_cr.index.names)
 
-    n.plot(
+    bus_sizes_cr = bus_sizes_cr.groupby(level = [0,1]).sum()
+
+    network.plot(
         ax=ax,
         bus_sizes=bus_sizes_cr * bus_scale,
         link_widths=link_width,
@@ -22,24 +31,25 @@ def plot_EnBalance(n, carrier, ax, bus_sizes, bus_scale = 3e-10, title = 'En bal
 
     # Anootation text with bus names, offset a bit
     offset = 0.00015  
-    for name, row in n.buses.loc[cr_buses].iterrows():
+    for name, row in network.buses.loc[bus_sizes_cr.index.get_level_values(0).unique()].iterrows():
         ax.text(
             row.x + offset,
             row.y + offset,
-            name,
-            fontsize=8
+            name.strip('_LT'),
+            fontsize=14,
+            zorder = 10000
         )
     handles = []
 
     # Creating legends based on carriers
-    for carrier in bus_sizes.index.get_level_values(1).unique():
+    for carrier in bus_sizes_cr.index.get_level_values(1).unique():
         handles.append(
             plt.Line2D(
                 [],
                 [],
                 marker="o",
                 linestyle="",
-                color=n.carriers.loc[carrier, "color"],
+                color=network.carriers.loc[carrier, "color"],
                 label=carrier
             )
         )
