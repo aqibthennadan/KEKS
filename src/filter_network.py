f7b3de9f1034ff0022e2212f25ca51bbe40c0ccf
Aqib Thennadan <aqib.thennadan@hs-offenburg.de>
2026-08-06 16:12:34 +0200

    changed export calculation to updated mehtod using gen with negative power

--- src/filter_network.py
+++ src/filter_network.py
@@ -40,15 +40,15 @@ def calc_bus_sizes(n):
     )
 
     ##considering only the charging into the export sink, and not the discharging from it, which is the export to grid. 
-    export = pd.Series(
-        {
-            ("Extern_grid", n.stores.loc['Export_sink', 'carrier']): -1 * n.stores_t.p["Export_sink"][n.stores_t.p["Export_sink"] < 0].sum() 
-        }
-    )
+    # export = pd.Series(
+    #     {
+    #         ("Extern_grid", n.stores.loc['Export_sink', 'carrier']): -1 * n.stores_t.p["Export_sink"][n.stores_t.p["Export_sink"] < 0].sum() 
+    #     }
+    # )
 
     bus_sizes = pd.concat([gen_bus_sizes, -1 * link_bus_sizes, -1 * link2_bus_sizes])
     bus_sizes = bus_sizes[bus_sizes > 1e-6] #removing zeroes and very small values
-    bus_sizes = pd.concat([bus_sizes, export])
+    # bus_sizes = pd.concat([bus_sizes, export])
 
     bus_sizes_norm = bus_sizes/bus_sizes.groupby(level = 0).sum()
 
